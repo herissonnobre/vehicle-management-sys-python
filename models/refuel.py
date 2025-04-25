ec7a901d3e9b78228d1f507d8d42a4a965abf62d
Herisson Nobre <herisson.carvalho96@gmail.com>
2025-04-25 01:01:58 -0300

    feat: refuels log

---
+++ models/refuel.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class RefuelRecord:
+    date: str
+    odometer: int
+    fuel_tipe: str
+    total_value: Optional[float] = None
+    price_per_liter: Optional[float] = None
+    liters: Optional[float] = None
+
+    def complete_data(self):
+        if self.total_value is None and self.price_per_liter is not None and self.liters is not None:
+            self.total_value = round(self.price_per_liter * self.liters, 2)
+        elif self.price_per_liter is None and self.total_value is not None and self.liters is not None:
+            self.price_per_liter = round(self.total_value / self.liters, 2)
+        elif self.liters is None and self.total_value is not None and self.price_per_liter is not None:
+            self.liters = round(self.total_value / self.price_per_liter, 2)
